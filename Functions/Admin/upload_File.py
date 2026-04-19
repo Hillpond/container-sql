04a1084a32a445bd8cb01c7f9165755ce13e9ebb
Kristian Tjernshaugen Husby <35239676+Hillpond@users.noreply.github.com>
2026-04-19 13:18:44 +0200

    #15 fixed names

--- Functions/Admin/upload_File.py
+++ Functions/Admin/upload_File.py
@@ -5,9 +5,9 @@ from fastapi import UploadFile
 def upload_files(file: UploadFile):
     save_path = os.path.join("Functions", "Admin", "DB's", file.filename)
 
-    with open(save_path, "wb") as file:
+    with open(save_path, "wb") as loaclFile:
         contents =  file.read()
-        file.write(contents)
+        loaclFile.write(contents)
 
     return {"message": f"{file.filename} lastet opp"}
 
