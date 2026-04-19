840e73b671f6df75e43597f077e626183b21330d
Kristian Tjernshaugen Husby <35239676+Hillpond@users.noreply.github.com>
2026-04-19 12:10:24 +0200

    Made controller

--- test.py
+++
@@ -1,5 +0,0 @@
-import docker
-
-client = docker.DockerClient(base_url='npipe:////./pipe/docker_engine')
-output = client.containers.run("alpine", ["echo", "hello", "world"])
-print(output.decode())
