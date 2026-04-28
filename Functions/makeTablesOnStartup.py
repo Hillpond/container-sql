be6693024f360a6994cb4db033eebe6b117e089b
Kristian Tjernshaugen Husby <35239676+Hillpond@users.noreply.github.com>
2026-04-28 09:01:22 +0200

    added something

--- Functions/makeTablesOnStartup.py
+++ Functions/makeTablesOnStartup.py
@@ -4,22 +4,23 @@ import asyncio
 from Functions.Admin.getSelectedFile import getSelectedFile
 
 #oppretter kobling til databasen
-mydb = mysql.connector.connect(
-  host="localhost",
-  port="3307",
-  user="root",
-  passwd="pass"
-)
-
-# lager et cursor objekt, funker litt som hvor / hva man gjør i databasen
-cursor = mydb.cursor()
-
-#Variables
-
-
+def connectToMySQL():
+  try:
+    mydb = mysql.connector.connect(
+      host="localhost",
+      port=3307,
+      user="root",
+      passwd="pass"
+    )
+    return mydb
+  except Exception as e:
+    return "Error connecting to MySQL"
 
 #making tables form sql file:
 async def makeTablesOnStartup(userSchemaName):
+  mydb = connectToMySQL()
+  # lager et cursor objekt, funker litt som hvor / hva man gjør i databasen
+  cursor = mydb.cursor()
   selectedFile = await getSelectedFile()
   pathToSchemaScript = "Functions/Admin/DB's/" + selectedFile
   cursor.execute(f"USE {userSchemaName}")  # Select database ur working in (need to get the info from local session)
